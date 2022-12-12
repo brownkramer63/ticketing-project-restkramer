6b11ed1a6dc41df99708fc689e4fee23d9dd66d2
Kramer Brown <brownkramer63@gmail.com>
2022-12-12 12:34:46 -0600

    keycloak

--- src/main/java/com/cydeo/TicketingProjectRestApplication.java
+++ src/main/java/com/cydeo/TicketingProjectRestApplication.java
@@ -3,6 +3,7 @@ package com.cydeo;
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
